2d9fd98b830f9904f810ce0b7ea53ebb24b80d23
Måns Dahlström <mans.dahlstrom@volvocars.com>
2023-08-25 10:22:07 +0200

    Add basic middleware

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import "server-only";
+
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: "/:path*",
+};
