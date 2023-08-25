efbab3d9e35b9617d5b18cf7ee1543cc52943e7d
Måns Dahlström <mans.dahlstrom@volvocars.com>
2023-08-25 10:32:43 +0200

    Test with cookieService

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
-import "server-only";
-
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { functionThatChecksAccessCookies } from "./services/cookieService";
 
 export function middleware(request: NextRequest) {
+  const someCookie = functionThatChecksAccessCookies();
   return NextResponse.next();
 }
 
---
+++ src/services/cookieService.ts
@@ -0,0 +1,8 @@
+import { cookies } from "next/headers";
+
+export const functionThatChecksAccessCookies = () => {
+  const cookieStore = cookies();
+  const theme = cookieStore.get("someCookie");
+
+  return theme;
+};
